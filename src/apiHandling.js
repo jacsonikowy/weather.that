3edca78584229d7abd932d0310665a445306d531
jacsonikowy <michal6345@wp.pl>
2022-09-25 12:38:02 +0200

    Handling api in second .js file and linted using ESLint

---
+++ src/apiHandling.js
@@ -0,0 +1,62 @@
+const API_KEY = 'cf1262a2bcbb31bb02c713662121cd2d';
+
+let cityName = '';
+let lat = 0;
+let lon = 0;
+
+const temperature = document.querySelector('.temperature');
+const humidity = document.querySelector('.humidity');
+const feelsLike = document.querySelector('.feels-like');
+const windSpeed = document.querySelector('.wind-speed');
+const pressure = document.querySelector('.pressure');
+const weatherDesc = document.querySelector('.weather');
+const cityDesc = document.querySelector('.city');
+
+export function displayIcons(response) {
+  const image = document.querySelector('.weather-img');
+
+  const url = `http://openweathermap.org/img/wn/${response.weather[0].icon}@4x.png`;
+
+  image.src = url;
+}
+
+export function gettingApiData(latitude, longitude) {
+  fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`, { mode: 'cors' })
+    .then((response) => {
+      console.log('Successfully fetched!');
+      return response.json();
+    })
+    .then((response) => {
+      weatherDesc.textContent = response.weather[0].description;
+      cityDesc.textContent = cityName;
+      temperature.textContent = `${parseInt(response.main.temp, 10)}°C`;
+      feelsLike.textContent = `Feels Like: ${parseInt(response.main.feels_like, 10)}°C`;
+      humidity.textContent = `Humidity: ${response.main.humidity}%`;
+      windSpeed.textContent = `Wind Speed: ${response.wind.speed}m/s`;
+      pressure.textContent = `Pressure: ${response.main.pressure}hPa`;
+
+      displayIcons(response);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+export function getLatAndLon(city) {
+  fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${API_KEY}`, { mode: 'cors' })
+    .then((response) => {
+      console.log('Data successfully fetched!');
+      return response.json();
+    })
+    .then((response) => {
+      console.log(response);
+      cityName = response[0].name;
+      lat = response[0].lat;
+      lon = response[0].lon;
+
+      gettingApiData(lat, lon);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
--- src/index.js
+++ src/index.js
@@ -1,140 +1,74 @@
-const API_KEY = "YOUR_API_KEY"
+import * as handle from './apiHandling.js';
 
 // html paragraphs for information
-const weather_desc = document.querySelector(".weather")
-const city_desc = document.querySelector(".city")
-const date_desc = document.querySelector(".date")
-const hour_desc = document.querySelector(".hour")
-
-const temperature = document.querySelector(".temperature")
-
-const humidity = document.querySelector(".humidity")
-const feelsLike = document.querySelector(".feels-like")
-const windSpeed = document.querySelector(".wind-speed")
-const pressure = document.querySelector(".pressure")
-
-
-var cityName = ""
-var lat = 0;
-var lon = 0;
-
-// on Load
-window.addEventListener("load", (e) => {
-	startup()
-})
-
-// Listeners
-const searchBtn = document.querySelector(".searchBtn")
-const locationSearch = document.querySelector(".locationSearch") 
-
-locationSearch.addEventListener("keypress", (e) => {
-	if(e.key === "Enter"){
-		getLatAndLon(locationSearch.value)
-		displayDateAndTime()
-	}
-})
-
-searchBtn.addEventListener("click", (e) => {
-	if(e.target){
-		getLatAndLon(locationSearch.value)
-		displayDateAndTime()
-	}
-})
+const dateDesc = document.querySelector('.date');
+const hourDesc = document.querySelector('.hour');
 
 // Functions
-function startup(){
-		getLatAndLon("Auckland")
-		displayDateAndTime()
+function displayDateAndTime() {
+  const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+
+  const weekdays = [
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+    'Sunday',
+  ];
+
+  const date = new Date();
+
+  const day = date.getDate();
+  const weekday = weekdays[date.getDay()];
+  const month = months[date.getMonth()];
+  const year = date.getFullYear();
+
+  const hour = date.getHours();
+  const minute = date.getMinutes();
+
+  dateDesc.textContent = `${weekday}, ${day}th ${month} ${year}`;
+  hourDesc.textContent = `${hour}:${minute}`;
 }
 
-function getLatAndLon(city){
-	fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${API_KEY}`, {mode: 'cors'})
-		.then(response => {
-			console.log("Data successfully fetched!")
-			return response.json()
-		})
-		.then(response => {
-			console.log(response)
-			cityName = response[0].name
-			lat = response[0].lat
-			lon = response[0].lon
-
-			gettingApiData(lat,lon)
-		})
-		.catch(error => {
-			console.log("Something wrong happened")
-		});
-
+function startup() {
+  handle.getLatAndLon('Auckland');
+  displayDateAndTime();
 }
 
-function gettingApiData(lat,lon){
-	fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`, {mode: 'cors'})
-		.then(response => {
-			console.log("Successfully fetched!")
-			return response.json()
-		})
-		.then(response => {
-			console.log(response)
-			weather_desc.textContent = response.weather[0].description
-			city_desc.textContent = cityName
-			temperature.textContent = parseInt(response.main.temp) + "°C"
-			feelsLike.textContent = "Feels Like: " + parseInt(response.main.feels_like) + "°C"
-			humidity.textContent = "Humidity: " + response.main.humidity + "%"
-			windSpeed.textContent = "Wind Speed: " + response.wind.speed + "m/s"
-			pressure.textContent = "Pressure: " + response.main.pressure + "hPa"
+// on Load
+window.addEventListener('load', () => {
+  startup();
+});
 
-			displayIcons(response)
-		})
-		.catch(error => {
-			console.log("Something Wrong happened")
-		})
-}
+// Listeners
+const searchBtn = document.querySelector('.searchBtn');
+const locationSearch = document.querySelector('.locationSearch');
 
-function displayIcons(response){
-	const image = document.querySelector(".weather-img")
+locationSearch.addEventListener('keypress', (e) => {
+  if (e.key === 'Enter') {
+    handle.getLatAndLon(locationSearch.value);
+    displayDateAndTime();
+  }
+});
 
-	let url = `http://openweathermap.org/img/wn/${response.weather[0].icon}@4x.png`
-
-	image.src=url
-}
-
-function displayDateAndTime(){
-	const months = [
-		"January",
-		"February",
-		"March",
-		"April",
-		"May",
-		"June",
-		"July",
-		"August",
-		"September",
-		"October",
-		"November",
-		"December"
-	]
-
-	const weekdays = [
-		"Monday",
-		"Tuesday",
-		"Wednesday",
-		"Thursday",
-		"Friday",
-		"Saturday",
-		"Sunday"
-	]
-
-
-	const date = new Date()
-
-	const day = date.getDate()
-	const weekday = weekdays[date.getDay()]
-	const month = months[date.getMonth()]
-	const year = date.getFullYear()
-
-	const hour = date.getHours()
-	const minute = date.getMinutes()
-
-	date_desc.textContent = `${weekday}, ${day}th ${month} ${year}`
-	hour_desc.textContent = `${hour}:${minute}`
-}
+searchBtn.addEventListener('click', (e) => {
+  if (e.target) {
+    handle.getLatAndLon(locationSearch.value);
+    displayDateAndTime();
+  }
+});
